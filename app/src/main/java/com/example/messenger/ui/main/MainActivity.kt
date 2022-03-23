b332ef987d199929c3171215ea920b4db3f45a10
hojiakbarrr <hojiakbar_1997@mail.ru>
2022-03-23 22:29:40 +0600

    ...

--- app/src/main/java/com/example/messenger/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/messenger/ui/main/MainActivity.kt
@@ -3,21 +3,30 @@ package com.example.messenger.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.messenger.R
 import com.example.messenger.adapter.ViewPagerAdapter_Mian
 import com.example.messenger.databinding.ActivityMainBinding
+import com.example.messenger.model.User
 import com.example.messenger.ui.register.RegisterActivity
+import com.example.messenger.utils.toast
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     val auth = Firebase.auth
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-
+    var firebaseUser: FirebaseUser? = null
+    var reference: DatabaseReference? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        toolbar()
         setContentView(binding.root)
         actionBar?.hide()
         actionBar?.show()
@@ -41,7 +50,10 @@ class MainActivity : AppCompatActivity() {
             }.attach()
         }
         binding.logoutBtn.setOnClickListener {
+            val userId = auth.currentUser?.uid as String
+            FirebaseDatabase.getInstance().getReference("Users").child(userId).child("status").setValue("offline")
             auth.signOut()
+            toast("Good bye")
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
@@ -50,6 +62,30 @@ class MainActivity : AppCompatActivity() {
         val userId = intent.getSerializableExtra("user_id")
         val mailId = intent.getSerializableExtra("email_id")
 
+    }
+
+    private fun toolbar() {
+        firebaseUser = auth!!.currentUser
+
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser!!.uid)
+        reference?.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val user = snapshot.getValue(User::class.java)
+                binding.apply {
+                    username.text = user!!.userName
+                    if (user.imageUrl.isNullOrEmpty()) {
+                        binding.profileImage.setImageResource(R.drawable.ic_person)
+                    } else {
+                        Picasso.get().load(user.imageUrl).into(binding.profileImage)
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/example/messenger/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/messenger/ui/main/profile/ProfileFragment.kt
@@ -1,5 +1,9 @@
 package com.example.messenger.ui.main.profile
 
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,8 +11,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.messenger.R
+import com.example.messenger.databinding.ActivityMainBinding
+import com.example.messenger.databinding.ProfileFragmentBinding
+import com.example.messenger.model.User
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.OnProgressListener
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import com.squareup.picasso.Picasso
+import java.util.*
+import kotlin.collections.HashMap
+
+private const val IMAGE_REQUEST = 1
+
 
 class ProfileFragment : Fragment() {
+    private val binding: ProfileFragmentBinding by lazy {
+        ProfileFragmentBinding.inflate(layoutInflater)
+    }
+
+    private val binding_main: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     companion object {
         fun newInstance() = ProfileFragment()
@@ -16,11 +44,18 @@ class ProfileFragment : Fragment() {
 
     private lateinit var viewModel: ProfileViewModel
 
+    private var auth: FirebaseAuth? = null
+    var firebaseUser: FirebaseUser? = null
+    var reference: DatabaseReference? = null
+    var uri: Uri? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.profile_fragment, container, false)
+
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -29,4 +64,90 @@ class ProfileFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        profileInfo()
+
+
+    }
+
+    private fun profileInfo() {
+        auth = FirebaseAuth.getInstance()
+        firebaseUser = auth!!.currentUser
+
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser!!.uid)
+        reference?.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val user = snapshot.getValue(User::class.java)
+                binding.apply {
+                    nameUser.text = user!!.userName
+                    mailUser.text = user.email
+
+                    refreshImage(user)
+
+                    porfileImage.setOnClickListener {
+                        openGallery()
+
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    private fun refreshImage(user: User) {
+        if (user.imageUrl.isNullOrEmpty()) {
+            binding.porfileImage.setImageResource(R.drawable.ic_person)
+        } else {
+            Picasso.get().load(user.imageUrl).into(binding.porfileImage)
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
+            uri = data.data
+            uploadImage(uri)
+        }
+    }
+
+    private fun uploadImage(uri: Uri?) {
+        val reference: StorageReference =
+            FirebaseStorage.getInstance().getReference("images").child(UUID.randomUUID().toString())
+        val dialog = ProgressDialog(context)
+        dialog.setTitle("Loading...")
+        dialog.show()
+        if (uri != null) {
+            reference.putFile(uri)
+                .addOnSuccessListener(OnSuccessListener<Any?> {
+                    dialog.dismiss()
+                    reference.getDownloadUrl()
+                        .addOnSuccessListener(OnSuccessListener { uri -> updateUser(uri)
+                        })
+
+                }).addOnProgressListener { snapshot ->
+                    val progress: Double =
+                        100.0 * snapshot.bytesTransferred / snapshot.totalByteCount
+                    dialog.setMessage("Uploaded" + progress.toInt() + "%")
+                }
+        }
+    }
+
+    private fun updateUser(uri: Uri?) {
+        val reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser!!.uid)
+        val map = HashMap<String, Any>()
+        map["imageUrl"] = uri.toString()
+        reference.updateChildren(map)
+    }
 }
